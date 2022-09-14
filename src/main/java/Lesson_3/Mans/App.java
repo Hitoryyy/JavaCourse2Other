de1c67ba4d9914c4aa8d809f897121c843bdfbf1
Hitory <hitory@yahoo.com>
2022-09-14 16:11:16 +0300

    | Урок № 3 и задачи со статическими методами из JavaRush. | 14.09.2022 | 16:11 |

---
+++ src/main/java/Lesson_3/Mans/App.java
@@ -0,0 +1,14 @@
+package Lesson_3.Mans;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Man man = new Man(1111222233);
+
+        Man man1 = new Man(1111222233);
+
+        System.out.println(man1.equals(man));
+    }
+
+}
