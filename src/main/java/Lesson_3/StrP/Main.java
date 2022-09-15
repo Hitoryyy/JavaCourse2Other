b49f135fa2cbf396ae75d38dcc6e62faeeb89b24
Hitory <hitory@yahoo.com>
2022-09-15 12:38:03 +0300

    | Урок № 3 и задачи со статическими методами из JavaRush. | 15.09.2022 | 12:37 |

--- src/main/java/Lesson_3/StrP/Main.java
+++ src/main/java/Lesson_3/StrP/Main.java
@@ -1,15 +1,22 @@
 package Lesson_3.StrP;
 
+/*
+    Сравнение строк в JAVA
+ */
+
 public class Main {
 
     public static void main(String[] args) {
 
+        /*--------------------- ==  --------------------------------------------*/
         String s1 = "Java - лучший язык программирования!";
         String s2 = "Java - лучший язык программирования!";
         String s3 = new String("Java - лучший язык программирования!");
 
         System.out.println(s1 == s2);
         System.out.println(s1 == s3 || s2 == s3);
+
+        /*----------------------------- equals()    -----------------------------*/
 
         String s4 = "Java - лучший язык программирования!";
         String s5 = "Java - лучший язык программирования!";
@@ -27,6 +34,13 @@ public class Main {
         System.out.println(s7.equalsIgnoreCase(s8));
         /*---------------------------------------------------------------------*/
 
+        System.out.println();
+        System.out.println("intern() :");
 
+        /*----------------------    intern()    -------------------------------*/
+        String s9 = "Java";
+        String s10 = new String("Java");
+        System.out.println(s9 == s10.intern());
+        /*---------------------------------------------------------------------*/
     }
 }
